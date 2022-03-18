9d761350ad6dc7c1c908aab57bb9319a05e0d0e6
Adam Łapiński <adam.lapinski@polarnightsoftware.com>
2022-03-18 13:49:58 +0100

    Merged feat/update_service_worker_to_cache_svg_files into master

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -9,7 +9,6 @@
 // service worker, and the Workbox build step will be skipped.
 
 import { clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
@@ -53,21 +52,14 @@ registerRoute(
   createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
 );
 
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
   ({ url }) =>
     url.origin === self.location.origin &&
-    (url.pathname.endsWith(".png") || url.pathname.endsWith(".ico")),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    (url.pathname.endsWith(".png") ||
+      url.pathname.endsWith(".ico") ||
+      url.pathname.endsWith(".ico")),
   new StaleWhileRevalidate({
     cacheName: "images",
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
-    ],
   })
 );
 
