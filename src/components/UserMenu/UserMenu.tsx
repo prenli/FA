fef569efb7611e574740f6e73c7b0819f6935bce
Adam Łapiński <adam.lapinski@polarnightsoftware.com>
2022-03-23 15:14:43 +0100

    Merged feat/change_width_of_user_menu into master

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,19 +1,19 @@
+import { ReactNode } from "react";
 import { Menu, Transition } from "@headlessui/react";
+import { ReactComponent as LogoutIcon } from "assets/logout.svg";
+import { ReactComponent as UserCircleIcon } from "assets/user-circle.svg";
 import classNames from "classnames";
-import { useTranslation } from "react-i18next";
-import { ReactComponent as LogoutIcon } from "../../assets/logout.svg";
-import { ReactComponent as UserCircleIcon } from "../../assets/user-circle.svg";
-import { keycloakService } from "../../services/keycloakService";
+import { TranslationText } from "components";
+import { keycloakService } from "services/keycloakService";
 
 const menuItems = [
   {
-    labelTranslationKey: "Logout",
+    label: <TranslationText translationKey="navTab.logout" />,
     action: () => keycloakService.onAuthLogout(),
   },
 ];
 
 export const UserMenu = () => {
-  const { t } = useTranslation();
   return (
     <Menu as="div" className="inline-block relative">
       <Menu.Button>
@@ -29,13 +29,9 @@ export const UserMenu = () => {
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
       >
-        <Menu.Items className="absolute right-0 z-10 py-1 w-52 bg-white rounded-md divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 py-1 bg-white rounded-md divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none min-w-[120px]">
           {menuItems.map((item, index) => (
-            <MenuItem
-              key={index}
-              label={t(item.labelTranslationKey)}
-              action={item.action}
-            />
+            <MenuItem key={index} label={item.label} action={item.action} />
           ))}
         </Menu.Items>
       </Transition>
@@ -44,7 +40,7 @@ export const UserMenu = () => {
 };
 
 interface MenuItemProps {
-  label: string;
+  label: ReactNode;
   action: () => void;
 }
 
