bd2d0d798bb5c09335fc279acb987b4bd8e3332f
Adam Łapiński <adam.lapinski@polarnightsoftware.com>
2022-04-08 06:21:16 +0000

    fix: display datepicker on ios

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from "react";
+import { createPopper } from "@popperjs/core";
 import { ReactComponent as CalendarIcon } from "assets/calendar.svg";
 import { useModifiedTranslation } from "hooks/useModifiedTranslation";
 import ReactDatePicker, {
@@ -10,11 +12,15 @@ interface DatePickerProps extends ReactDatePickerProps {
   label?: string;
 }
 
+// TODO: keep eye on react-date-picker they plan to add portalContainer prop which will simplify calendar positioning
 export const DatePicker = ({ label, ...props }: DatePickerProps) => {
   const { i18n } = useModifiedTranslation();
 
+  const { positionedElementRefCallback, targetRefCallback } =
+    usePositionElementToOtherElement();
+
   return (
-    <div className="flex flex-col gap-0 w-full ">
+    <div className="flex flex-col gap-0 w-full" ref={targetRefCallback}>
       {label && (
         <label className="mb-1 text-sm font-medium text-gray-900">
           {label}
@@ -23,11 +29,41 @@ export const DatePicker = ({ label, ...props }: DatePickerProps) => {
       <ReactDatePicker
         calendarIcon={<CalendarIcon />}
         clearIcon={null}
-        className="isolate py-3 px-3.5 text-base font-normal leading-tight text-gray-500 bg-gray-50 rounded-lg border border-gray-200"
+        className="py-3 px-3.5 text-base font-normal leading-tight text-gray-500 bg-gray-50 rounded-lg border border-gray-200"
         locale={i18n.language}
         showLeadingZeros
+        inputRef={positionedElementRefCallback}
         {...props}
       />
     </div>
   );
+};
+
+const usePositionElementToOtherElement = () => {
+  const [targetNode, setTargetNode] = useState<HTMLElement>();
+
+  const targetRefCallback = useCallback((node: HTMLElement | null) => {
+    if (!node) {
+      return;
+    }
+    setTargetNode(node);
+  }, []);
+
+  const positionedElementRefCallback = useCallback(
+    (node: HTMLElement | null) => {
+      if (!node || !targetNode) {
+        return;
+      }
+      console.log(targetNode, node);
+      createPopper(targetNode, node, {
+        placement: "bottom-start",
+      });
+    },
+    [targetNode]
+  );
+
+  return {
+    positionedElementRefCallback,
+    targetRefCallback,
+  };
 };
