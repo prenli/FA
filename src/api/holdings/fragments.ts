a77d79eb7e3095f04bdc28d9de067826d9e9d5dd
Adam Lapinski <a.f.lapinski@gmail.com>
2022-05-26 13:58:27 +0200

    fix: add portfolioId to portfolioReportItems in all portfolios query

--- src/api/holdings/fragments.ts
+++ src/api/holdings/fragments.ts
@@ -47,3 +47,34 @@ export const PORTFOLIO_REPORT_HOLDINGS_DETAILS_FIELDS = gql`
     }
   }
 `;
+
+// all portfolios does not have identifier for holdingPositions (portfolioID returns id of first portfolio)
+// this is why we use
+// portfolioId: portfolio {
+//   id
+//   contact {
+//     id
+//   }
+// }
+export const ALL_PORTFOLIOS_REPORT_HOLDINGS_DETAILS_FIELDS = gql`
+  fragment AllPortfoliosReportHoldingDetailsFields on PortfolioReport {
+    holdingPositions: portfolioReportItems {
+      portfolioId: portfolio {
+        id
+        contact {
+          id
+        }
+      }
+      security {
+        id
+        securityCode
+      }
+      amount
+      accruedInterest
+      purchaseTradeAmount
+      marketValue
+      valueChangeAbsolute
+      valueChangeRelative
+    }
+  }
+`;
