5fe605b23e4a425a84da811ebff6ce2bb4c21a73
Adam Lapinski <a.f.lapinski@gmail.com>
2022-06-09 14:19:26 +0200

    feat: use popper to display options list in Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -3,9 +3,10 @@ import { Listbox, Transition } from "@headlessui/react";
 import { ReactComponent as ChevronDown } from "assets/chevron-down.svg";
 import classNames from "classnames";
 import { useModifiedTranslation } from "hooks/useModifiedTranslation";
+import { usePopper } from "../../hooks/usePopper";
 
 export interface Option {
-  id: number | string;
+  id: number | string | null;
   label: string;
   OptionComponent?: ReactNode;
 }
@@ -24,45 +25,70 @@ export const Select = <TOption extends Option>({
   label,
 }: SelectProps<TOption>) => {
   const { t } = useModifiedTranslation();
+  const [trigger, container] = usePopper({
+    placement: "bottom-start",
+    modifiers: [
+      {
+        name: "sameWidth",
+        enabled: true,
+        phase: "beforeWrite",
+        fn({ state }) {
+          state.styles.popper.width = `${state.rects.reference.width}px`;
+        },
+        requires: ["computeStyles"],
+        effect: ({ state }) => {
+          if (state.elements.reference instanceof Element) {
+            state.elements.popper.style.width = `${state.elements.reference.clientWidth}px`;
+          }
+        },
+      },
+    ],
+  });
   return (
     <Listbox as="div" value={value} onChange={onChange}>
       {label && (
         <Listbox.Label className="text-sm font-normal">{label}</Listbox.Label>
       )}
-      <Listbox.Button className="flex items-center py-2.5 px-4 w-full h-10 text-lg font-bold text-gray-900 bg-gray-50 rounded-lg border border-gray-300">
-        <div className="box-border flex flex-1 content-start leading-none">
+      <Listbox.Button
+        className="flex gap-2 items-center py-2.5 px-4 w-full h-10 text-lg font-bold text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+        ref={trigger}
+      >
+        <div className="box-border flex-1 content-start leading-none text-left truncate">
           {value?.label ?? t("component.select.placeholder")}
         </div>
         <ChevronDown className="stroke-gray-500 w-[20px] h-[20px]" />
       </Listbox.Button>
-      <Transition
-        enter="transition duration-100 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-95 opacity-0"
-      >
-        <Listbox.Options className="absolute inset-x-0 z-10 py-1 text-base list-none bg-white dark:bg-gray-700 rounded divide-y divide-gray-100 shadow ">
-          {options.map((option) => (
-            <Listbox.Option key={option.id} value={option} as={Fragment}>
-              {({ active, selected }) => (
-                <li
-                  className={classNames(
-                    "block py-2 px-4 text-sm text-gray-700 dark:text-gray-200 ",
-                    {
-                      "dark:text-white bg-primary-50 dark:bg-gray-600": active,
-                      "font-bold": selected,
-                    }
-                  )}
-                >
-                  {option.OptionComponent ?? option.label}
-                </li>
-              )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Transition>
+      <div ref={container}>
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <Listbox.Options className="py-1 text-base list-none bg-white rounded divide-y divide-gray-100 shadow ">
+            {options.map((option) => (
+              <Listbox.Option key={option.id} value={option} as={Fragment}>
+                {({ active, selected }) => (
+                  <li
+                    className={classNames(
+                      "block py-2 px-4 text-sm text-gray-700 dark:text-gray-200 ",
+                      {
+                        "dark:text-white bg-primary-50 dark:bg-gray-600":
+                          active,
+                        "font-bold": selected,
+                      }
+                    )}
+                  >
+                    {option.OptionComponent ?? option.label}
+                  </li>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Transition>
+      </div>
     </Listbox>
   );
 };
---
+++ src/hooks/usePopper.ts
@@ -0,0 +1,44 @@
+import { RefCallback, useRef, useCallback, useMemo } from "react";
+import { createPopper, Options } from "@popperjs/core";
+
+export const usePopper = (
+  options?: Partial<Options>
+): [RefCallback<Element | null>, RefCallback<HTMLElement | null>] => {
+  const reference = useRef<Element | null>(null);
+  const popper = useRef<HTMLElement | null>(null);
+
+  const cleanupCallback = useRef(() => {
+    return;
+  });
+
+  const instantiatePopper = useCallback(() => {
+    if (!reference.current) {
+      return;
+    }
+    if (!popper.current) {
+      return;
+    }
+
+    if (cleanupCallback.current) cleanupCallback.current();
+
+    cleanupCallback.current = createPopper(
+      reference.current,
+      popper.current,
+      options
+    ).destroy;
+  }, [reference, popper, cleanupCallback, options]);
+
+  return useMemo(
+    () => [
+      (referenceDomNode) => {
+        reference.current = referenceDomNode;
+        instantiatePopper();
+      },
+      (popperDomNode) => {
+        popper.current = popperDomNode;
+        instantiatePopper();
+      },
+    ],
+    [reference, popper, instantiatePopper]
+  );
+};
