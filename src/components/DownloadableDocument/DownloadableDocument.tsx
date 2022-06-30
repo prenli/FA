735a0aa4cdd653e1a298bce89292d98e3734b9da
Adam Lapinski <a.f.lapinski@gmail.com>
2022-06-30 12:02:57 +0200

    feat: change DownloadableDocument icon

--- src/components/DownloadableDocument/DownloadableDocument.tsx
+++ src/components/DownloadableDocument/DownloadableDocument.tsx
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from "react";
-import { ReactComponent as LinkIcon } from "assets/external-link.svg";
+import { ReactComponent as FileIcon } from "assets/document-text.svg";
 
 interface DownloadableDocumentProps extends HTMLAttributes<HTMLAnchorElement> {
   label: string;
@@ -12,9 +12,9 @@ export const DownloadableDocument = ({
   ...anchorAttributes
 }: DownloadableDocumentProps) => (
   <a target="_blank" rel="noopener noreferrer" {...anchorAttributes} href={url}>
-    <div className="flex justify-between text-primary-600">
+    <div className="flex justify-between text-primary-600 stroke-primary-600">
       <div className="text-base font-semibold">{label}</div>
-      <LinkIcon className="stroke-primary-600" />
+      <FileIcon className="w-6 h-6 stroke-primary-600" />
     </div>
   </a>
 );
