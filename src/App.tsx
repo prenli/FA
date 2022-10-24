759c565acbe2c19a64ef670ddabeb3e341f56ccd
Patrik Wingqvist <patrik@fasolutions.com>
2022-10-24 16:10:30 +0200

    Now booting intercom chat from MainLayout and providing name and representative to intercom.

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import { InitialLanguageProvider } from "providers/InitialLanguageProvider";
 import { KeycloakProvider } from "providers/KeycloakProvider";
 import { PersistedApolloProvider } from "providers/PersistedApolloProvider";
 import { ServiceWorkerRegistrationProvider } from "providers/ServiceWorkerRegistrationProvider";
+import { IntercomProvider } from 'react-use-intercom';
 import { keycloakService } from "services/keycloakService";
 import { Toast } from "./components";
 import { UserWithLinkedContactRoutes } from "./pages/userWithLinkedContact/routes";
 import "react-toastify/dist/ReactToastify.css";
 import "styles/fonts.css";
+
+const INTERCOM_APP_ID = 'gkgnpk1g';
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
       <ServiceWorkerRegistrationProvider>
         <ErrorBoundary>
           <KeycloakProvider keycloak={keycloakService}>
+          <IntercomProvider 
+            appId={INTERCOM_APP_ID}
+          >
             <DetailProvider>
               <PersistedApolloProvider>
                 <APILanguageProvider>
@@ -26,6 +32,7 @@ function App() {
                 </APILanguageProvider>
               </PersistedApolloProvider>
             </DetailProvider>
+          </IntercomProvider>
           </KeycloakProvider>
           <Toast />
         </ErrorBoundary>
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,9 +1,32 @@
 import { Suspense } from "react";
 import { LoadingIndicator } from "components";
 import { ErrorBoundary } from "components/ErrorBoundary/ErrorBoundary";
+import { useGetContractIdData } from "providers/ContractIdProvider";
+import { useKeycloak } from "providers/KeycloakProvider";
 import { Outlet } from "react-router-dom";
+import { useIntercom } from 'react-use-intercom';
+import { useGetContactInfo } from "../../api/initial/useGetContactInfo";
 
 export const MainLayout = () => {
+
+  //Setting up Intercom chat properties
+  const {linkedContact} = useKeycloak()
+  console.log("Linked contact id", linkedContact)
+  const { selectedContact } = useGetContractIdData();
+  console.log("Linked contact object", selectedContact)
+  const contactDataFromFA = useGetContactInfo(true,linkedContact)
+  const contactRepresentatives = contactDataFromFA.data?.representees
+  const representativeName = contactRepresentatives && contactRepresentatives[0] && contactRepresentatives[0].name
+  console.log("representativeName", representativeName)
+  const contactName = contactDataFromFA.data?.name
+  const { boot} = useIntercom();
+
+  boot({
+    //TODO: Add email, phone
+    name: contactName,
+    customAttributes: { representativeName: representativeName},
+  })
+
   return (
     <div className="box-border text-gray-900 bg-gray-50 h-[calc(100vh-0.5rem)]">
       <ErrorBoundary>
